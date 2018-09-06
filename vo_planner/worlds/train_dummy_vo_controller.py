77e7c08ad74b627f0a39018ca1d8d1640a25affd
johannah <jh1736@gmail.com>
2018-09-06 01:53:30 -0400

    added velocity to state predictions. and it seems to improve predict

--- vo_planner/worlds/train_dummy_vo_controller.py
+++ vo_planner/worlds/train_dummy_vo_controller.py
@@ -97,9 +97,10 @@ def load_data(load_path):
     details = edetails[indexes]
     # change data type and shape, move from numpy to torch
     # note that we need to convert all data to np.float32 for pytorch
+    #   0    1     2    3      4    5   6     7    8      9         10
     # [pos0,pos1,speed,angle,steer,gas,brake,diffy,diffx,steering,throttle]
-    x_tensor = torch.from_numpy(np.float32(states[:-1,:,[3,4,9,10,7,8]]))
-    y_tensor = torch.from_numpy(np.float32(states[1:,:,[7,8]]))
+    x_tensor = torch.from_numpy(np.float32(states[:-1,:,[3,4,9,10,2,7,8]]))
+    y_tensor = torch.from_numpy(np.float32(states[1:,:,[2,7,8]]))
     x_variable = Variable(x_tensor, requires_grad=True)
     y_variable = Variable(y_tensor, requires_grad=False)
     return x_tensor, y_tensor
@@ -133,15 +134,17 @@ def predict(x, y):
     outputs = []
     input_data = x[0]
     # one batch of x
-    for i in np.arange(0,x.shape[0]-1):
+    for i in np.arange(x.shape[0]):
         output, h1_tm1, c1_tm1, h2_tm1, c2_tm1 = lstm(input_data, h1_tm1, c1_tm1, h2_tm1, c2_tm1)
         outputs+=[output]
-        input_data = x[i+1]
-        # replace the offset with the predicted offset
-        input_data[:,-2:] = output
-    # do final prediction
-    output, h1_tm1, c1_tm1, h2_tm1, c2_tm1 = lstm(input_data, h1_tm1, c1_tm1, h2_tm1, c2_tm1)
-    outputs+=[output]
+        if i < x.shape[0]-1:
+            input_data = x[i+1]
+            # replace the offset with the predicted offset
+            #if i < 10:
+            #    print(input_data[:,[4,5,6]])
+            #    print(output)
+            #    print('-------------',i+1)
+            input_data[:,[4,5,6]] = output
 
     y_pred = torch.stack(outputs, 0)
     #print(y_pred.shape)
@@ -156,14 +159,14 @@ def loop(xvar,yvar,num_epochs=1000,cnt=0,save_every=10000):
     last_save = cnt
     for e in range(num_epochs):
         ecnt = 0
-        if not e%1 and e>0:
-            tst = round((time.time()-st)/60., 0)
-            print("starting epoch %s, %s mins after start, loss %s, seen %s" %(e, tst, aloss[-1], cnt))
+        tst = round((time.time()-st)/60., 0)
+        if not e%10 and e>0:
+            print("starting epoch %s, %s mins, loss %s, seen %s, last save at %s" %(e, tst, aloss[-1], cnt, last_save))
         batch_loss = []
         for bst in np.arange(0, (xvar.shape[1]-batch_size)+1, batch_size, dtype=np.int):
             xd = xvar[:,bst:bst+batch_size].to(DEVICE)
             yd = yvar[:,bst:bst+batch_size].to(DEVICE)
-            if cnt-last_save > save_every:
+            if cnt-last_save >= save_every:
                 print("SAVE TRUE")
                 y_pred, mean_loss = train(xd, yd, cnt, True)
                 last_save = cnt
@@ -194,7 +197,6 @@ def valid_loop(function, xvar, yvar):
         xd = xvar[:,bst:bst+batch_size].to(DEVICE)
         yd = yvar[:,bst:bst+batch_size].to(DEVICE)
         y_pred, losses = function(xd, yd)
-
         predicts[:,bst:bst+batch_size,:] = y_pred.detach().numpy()
         trues[:,bst:bst+batch_size,:] = yd.detach().numpy()
         cnt+=batch_size
@@ -221,7 +223,6 @@ def plot_results(cnt, vx_tensor, vy_tensor, name='test'):
     for e in range(trues.shape[1]):
         #gt_y = np.cumsum(vy_tensor[:,e,-2].detach().numpy())
         #gt_x = np.cumsum(vy_tensor[:,e,-1].detach().numpy())
-
         ugty = np.cumsum(trues[:,e,0])
         ugtx = np.cumsum(trues[:,e,1])
         tfy = np.cumsum(tf_predicts[:,e,0])
@@ -229,25 +230,26 @@ def plot_results(cnt, vx_tensor, vy_tensor, name='test'):
         py = np.cumsum(predicts[:,e,0])
         px = np.cumsum(predicts[:,e,1])
 
-        xmin = np.min([px.min(), tfx.min(), ugtx.min()])
-        xmax = np.min([px.max(), tfx.max(), ugtx.max()])
-        ymin = np.min([py.min(), tfy.min(), ugty.min()])
-        ymax = np.max([py.max(), tfy.max(), ugty.max()])
+        xmin = np.min([px.min(), tfx.min(), ugtx.min()])-10
+        xmax = np.max([px.max(), tfx.max(), ugtx.max()])+10
+        ymin = np.min([py.min(), tfy.min(), ugty.min()])-10
+        ymax = np.max([py.max(), tfy.max(), ugty.max()])+10
         f,ax=plt.subplots(1,3, figsize=(9,3))
         ## original coordinates
-        ax[0].scatter(ugty,ugtx, c=np.arange(ugty.shape[0]))
+        ax[0].scatter(ugtx,ugty, c=np.arange(ugty.shape[0]))
         ax[0].set_xlim([xmin,xmax])
         ax[0].set_ylim([ymin,ymax])
         ax[0].set_title("target")
-        ax[1].scatter(tfy,tfx, c=np.arange(tfy.shape[0]))
+        ax[1].scatter(tfx,tfy, c=np.arange(tfy.shape[0]))
         ax[1].set_xlim([xmin,xmax])
         ax[1].set_ylim([ymin,ymax])
         ax[1].set_title('teacher force predict')
-        ax[2].scatter(py, px, c=np.arange(py.shape[0]))
+        ax[2].scatter(px, py, c=np.arange(py.shape[0]))
         ax[2].set_xlim([xmin,xmax])
         ax[2].set_ylim([ymin,ymax])
         ax[2].set_title('predict')
-        filename = os.path.join(img_savedir, 'model_epoch_%015d_%s_%05d.png'%(cnt,name,e))
+
+        filename = os.path.join(img_savedir, '%s_%s_%05d.png'%(model_save_name.replace('.pkl',''),name,e))
         plt.savefig(filename)
         plt.close()
 
@@ -255,22 +257,19 @@ if __name__ == '__main__':
     import argparse
     hidden_size = 1024
     batch_size = 10
-    output_size = 2
     savedir = 'models'
     img_savedir = 'predictions'
     cnt = 0
-    model_save_name = 'model'
-    model_load_path = 'models/model_epoch_000000100000150.pkl'
+    model_load_path = 'models/model_limit_0050_000000000005000.pkl'
     if not os.path.exists(savedir):
         os.makedirs(savedir)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--cuda', action='store_true', default=False)
-    parser.add_argument('-dp', '--do_plot', action='store_true', default=True)
-    parser.add_argument('-po', '--plot_only', action='store_true', default=False)
+    parser.add_argument('-po', '--plot', action='store_true', default=False)
     parser.add_argument('-m', '--model_loadname', default=model_load_path)
-    parser.add_argument('-se', '--save_every',
-                        default=1e7, help='how often to save training model')
+    parser.add_argument('-se', '--save_every',default=5e3, help='how often to save training model')
+    parser.add_argument('-ne', '--num_epochs',default=10000, help='num epochs to train')
     parser.add_argument('--limit', default=-1, type=int, help='limit training data to reduce convergence time')
     parser.add_argument('-l', '--load', action='store_true', default=False, help='load model to continue training or to generate. model path is specified with -m')
     parser.add_argument('-v', '--validate', action='store_true', default=False, help='test results')
@@ -282,8 +281,6 @@ if __name__ == '__main__':
     else:
         DEVICE = 'cpu'
 
-    if args.limit != -1:
-        model_save_name += "_limit_%04d"%args.limit
     mse_loss = nn.MSELoss().to(DEVICE)
 
     save_every = args.save_every
@@ -291,9 +288,15 @@ if __name__ == '__main__':
     x_tensor, y_tensor = load_data("train_2d_controller.npz")
     valid_x_tensor, valid_y_tensor = load_data("test_2d_controller.npz")
     input_size = x_tensor.shape[2]
+    output_size = y_tensor.shape[2]
     lstm = LSTM(input_size=input_size, output_size=output_size, hidden_size=hidden_size).to(DEVICE)
     optim = torch.optim.Adam(lstm.parameters(), lr=1e-5)
     limit = min(x_tensor.shape[1], args.limit)
+
+    model_save_name = 'model'
+    if args.limit != -1:
+        model_save_name += "_limit_%04d"%args.limit
+
     if args.load:
         if not os.path.exists(model_load_path):
             print("load model: %s does not exist"%model_load_path)
@@ -305,13 +308,12 @@ if __name__ == '__main__':
             optim.load_state_dict(lstm_dict['optimizer'])
             cnt = lstm_dict['epoch'] # epoch is actually count
             # todo losses
-
-    if not args.plot_only:
-        loop(x_tensor[:,:limit], y_tensor[:,:limit], save_every=save_every, cnt=cnt)
-
-    if args.do_plot or args.plot_only:
+    if not args.plot:
+        loop(x_tensor[:,:limit], y_tensor[:,:limit], save_every=save_every, cnt=cnt, num_epochs=args.num_epochs)
+    else:
+        model_save_name = os.path.split(model_load_path)[1]
         plot_results(cnt, valid_x_tensor[:,:batch_size], valid_y_tensor[:,:batch_size], name='test')
-        plot_results(cnt,       x_tensor[:,:batch_size],      y_tensor[:,:batch_size], name='train')
+        plot_results(cnt,       x_tensor[:,:batch_size],       y_tensor[:,:batch_size], name='train')
 
     embed()
 
