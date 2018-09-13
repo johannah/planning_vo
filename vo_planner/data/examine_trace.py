0f0ad7b84ef73e743c6cc2a27e516900c1d385af
johannah <jh1736@gmail.com>
2018-09-13 01:49:58 -0400

    added helper plotting functions for data

--- vo_planner/data/examine_trace.py
+++ vo_planner/data/examine_trace.py
@@ -4,10 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from IPython import embed
 import os, sys
-import pandas as pd
-
-raw = np.load('icra2019_vehicle_data_0643_files_2018-09-12.npz')
+from glob import glob
+search = 'icra2019_vehicle_data*.npz'
+# set to false to remove pandas dependency
+add_bagnames = True
+files = sorted(glob(search))
+if not len(files):
+    print("no files at", search)
+    sys.exit()
+else:
+   #raw = np.load('icra2019_vehicle_data_2018-09-12_0643_files.npz')
+    if not len(files):
+        print("found no files at search=%s" %search)
+        sys.exit()
+    fname = files[0]
+    raw = np.load(fname)
 states = raw['states']
+names = raw['bagnames']
 k = raw['keys'].item()
 
 good = []
@@ -22,43 +35,28 @@ def plot_trace(i, t, path):
     num_good = good.shape[0]
     ax[0].scatter(pred[:,0], pred[:,1], c='y', s=3, label='bad dso')
     ax[0].scatter(good[:,0], good[:,1], c='g', s=3, label='tracking dso')
-    #ax[0].legend()
+    ax[0].legend()
     aa = np.arange(true.shape[0])
-    ax[1].plot(aa,t[:,k['steering']], label='steer') 
-    ax[1].plot(aa,t[:,k['throttle']], label='throttle') 
-    plt.title("index %s  - %d good pts" %(i,num_good))
+    st=ax[1].plot(aa,t[:,k['steering']], label='steer')
+    le=ax[1].plot(aa,t[:,k['throttle']], label='throttle')
+    ax[1].legend()
+    plt.suptitle("index %s  - %d good pts" %(i,num_good))
     plt.savefig(path)
     plt.close()
     if num_good > 0 :
         print(path, num_good)
     return num_good
- 
 
-#    plt.figure() 
-#    true = t[:,[k['ml_x'], k['ml_y']]]
-#    pred = t[:,[k['dso_x'], k['dso_y']]]
-#    stat = t[:, k['dso_stat']].astype(np.int32)
-#    plt.plot(true[:,0], true[:,1], c='b', lw=3, label='true')
-#    good = pred[stat==1]
-#    num_good = good.shape[0]
-#    plt.scatter(pred[:,0], pred[:,1], c='y', s=3, label='bad dso')
-#    plt.scatter(good[:,0], good[:,1], c='g', s=3, label='tracking dso')
-#    plt.legend()
-#    plt.title("index %s  - %d good pts" %(i,num_good))
-#    plt.savefig(path)
-#    plt.close()
-#    if num_good > 0 :
-#        print(path, num_good)
-#    return num_good
-    
-pan = pd.read_csv('icra2019_vehicle_data_0642_files_2018-09-12.csv')
 img_dir = 'imgs'
 if not os.path.exists(img_dir):
     os.makedirs(img_dir)
 good_cnt = 0
 for i in range(states.shape[1]):
-    bagname = pan.loc[0,'bag_name']
-    fname = os.path.join(img_dir, '%strace_%04d.png' %(bagname,i))
+    if add_bagnames:
+        bagname = names[i]
+        fname = os.path.join(img_dir, '%strace_%04d.png' %(bagname,i))
+    else:
+        fname = os.path.join(img_dir, 'trace_%04d.png' %(i))
     good_cnt+=plot_trace(i, states[:,i], fname)
 print('all good', good_cnt)
 
--- vo_planner/data/search_data.py
+++ vo_planner/data/search_data.py
@@ -3,45 +3,49 @@ import numpy as np
 from glob import glob
 import os, sys
 import csv
+from copy import deepcopy
 from IPython import embed
 import pandas as pd
 from datetime import date
 
 base_dir = '/mnt/mllsynology/icra2019bags/'
-files = glob(os.path.join(base_dir, 'sept7', 
+files = glob(os.path.join(base_dir, 'sept7', 'Sep7_Train_data_step_size_02',
                           '*_ref', '*.csv'))
 print('found %s files' %len(files))
 # ts, dso_x, dso_y, sdso_z,  dso_qx, dso_qy, sdso_qz, sds_qw
 # define bags by hand so we know when they are messed up
-csv_columns = ['t', 'dso_x', 'dso_y', 'sdso_z', 
-               'dso_qx', 'dso_qy', 'dso_qz', 'dso_qw', 'dso_yaw', 'dso_stat', 
-               'ml_x', 'ml_y', 'ml_z', 
-               'ml_qx', 'ml_qy', 'ml_qz', 'ml_qw', 'ml_yaw', 
+csv_columns = ['t', 'dso_x', 'dso_y', 'sdso_z',
+               'dso_qx', 'dso_qy', 'dso_qz', 'dso_qw', 'dso_yaw', 'dso_stat',
+               'ml_x', 'ml_y', 'ml_z',
+               'ml_qx', 'ml_qy', 'ml_qz', 'ml_qw', 'ml_yaw',
                'steering', 'throttle', 'score', 'img_name'
                ]
-np_columns = ['dso_x', 'dso_y', 'sdso_z', 
-              'dso_qx', 'dso_qy', 'dso_qz', 'dso_qw', 'dso_yaw', 'dso_stat', 
-              'ml_x', 'ml_y', 'ml_z', 
-              'ml_qx', 'ml_qy', 'ml_qz', 'ml_qw', 'ml_yaw', 
+np_columns = ['dso_x', 'dso_y', 'sdso_z',
+              'dso_qx', 'dso_qy', 'dso_qz', 'dso_qw', 'dso_yaw', 'dso_stat',
+              'ml_x', 'ml_y', 'ml_z',
+              'ml_qx', 'ml_qy', 'ml_qz', 'ml_qw', 'ml_yaw',
               'steering', 'throttle', 'score'
                ]
 
 np_keys = {}
-for (c,k) in enumerate(np_columns): np_keys[k]=c 
+for (c,k) in enumerate(np_columns): np_keys[k]=c
 
 data = []
+names = []
 for cnt, f in enumerate(sorted(files)):
     readfile = pd.read_csv(f, names=csv_columns, skiprows=1)
     fname = os.path.split(f)[1]
+    print('working', fname)
     readfile['bag_name'] = fname
     data.append(np.array(readfile[np_columns]))
+    names.append(fname)
     if not cnt:
         out = readfile
     else:
-        out = out.append(readfile)
+        out = out.append(deepcopy(readfile))
 
-bname = 'icra2019_vehicle_data_%04d_files_%s' %(len(files), str(date.today()))
+bname = 'icra2019_vehicle_data_%s_%04d_files' %(str(date.today()), len(files))
 data = np.swapaxes(np.array(data),0,1)
 out.to_csv(bname+'.csv', header=True, index=True, index_label='N')
-np.savez(bname, states=data, keys=np_keys)
+np.savez(bname, states=data, keys=np_keys, bagnames=names)
 print('finished loading all files, data is shape', out.shape)
