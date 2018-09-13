c8eb75ab8b2fb0af3a0b55bc286145e12f993a78
johannah <jh1736@gmail.com>
2018-09-13 00:49:32 -0400

    keep search on track with mander

--- vo_planner/data/search_data.py
+++ vo_planner/data/search_data.py
@@ -1,26 +1,47 @@
+# Thanks to @https://github.com/weidi-chang for creating the core of this code
 import numpy as np
 from glob import glob
 import os, sys
 import csv
 from IPython import embed
+import pandas as pd
+from datetime import date
 
 base_dir = '/mnt/mllsynology/icra2019bags/'
-files = glob(os.path.join(base_dir, '*', '*cooked_data', '*',  '*.csv'))
+files = glob(os.path.join(base_dir, 'sept7', 
+                          '*_ref', '*.csv'))
 print('found %s files' %len(files))
+# ts, dso_x, dso_y, sdso_z,  dso_qx, dso_qy, sdso_qz, sds_qw
+# define bags by hand so we know when they are messed up
+csv_columns = ['t', 'dso_x', 'dso_y', 'sdso_z', 
+               'dso_qx', 'dso_qy', 'dso_qz', 'dso_qw', 'dso_yaw', 'dso_stat', 
+               'ml_x', 'ml_y', 'ml_z', 
+               'ml_qx', 'ml_qy', 'ml_qz', 'ml_qw', 'ml_yaw', 
+               'steering', 'throttle', 'score', 'img_name'
+               ]
+np_columns = ['dso_x', 'dso_y', 'sdso_z', 
+              'dso_qx', 'dso_qy', 'dso_qz', 'dso_qw', 'dso_yaw', 'dso_stat', 
+              'ml_x', 'ml_y', 'ml_z', 
+              'ml_qx', 'ml_qy', 'ml_qz', 'ml_qw', 'ml_yaw', 
+              'steering', 'throttle', 'score'
+               ]
+
+np_keys = {}
+for (c,k) in enumerate(np_columns): np_keys[k]=c 
 
 data = []
-for f in files:
-    with open(f, 'r') as fh:
-        episode = []
-        csv_reader = csv.reader(fh, delimiter=',')
-        n_lines = 0
-        next(csv_reader, None)
-        for row in csv_reader:
-            episode.append(row[1:-1])
-            n_lines +=1
-    data.append(episode)
+for cnt, f in enumerate(sorted(files)):
+    readfile = pd.read_csv(f, names=csv_columns, skiprows=1)
+    fname = os.path.split(f)[1]
+    readfile['bag_name'] = fname
+    data.append(np.array(readfile[np_columns]))
+    if not cnt:
+        out = readfile
+    else:
+        out = out.append(readfile)
 
-states = np.array(data, dtype=np.float32)
-states = np.swapaxes(states, 0, 1)
-np.savez('icra2019_vehicle_data', states=states)
-embed()
+bname = 'icra2019_vehicle_data_%04d_files_%s' %(len(files), str(date.today()))
+data = np.swapaxes(np.array(data),0,1)
+out.to_csv(bname+'.csv', header=True, index=True, index_label='N')
+np.savez(bname, states=data, keys=np_keys)
+print('finished loading all files, data is shape', out.shape)
